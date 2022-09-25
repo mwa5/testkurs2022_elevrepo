25769ad2b719d0fb1971c7db4cbb84663d1373c4
Mikael Waldenmark <mikael.waldenmark@amf.se>
2022-09-25 11:36:59 +0200

    Lade till mall för testfall med fake table

---
+++ Testkurs2022_tsqlt/tSQLt_Demo/testFinancialApp/Stored Procedures/test that ConvertCurrencyUsingLookup converts using conversion rate in CurrencyConversion table.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [testFinancialApp].[test that ConvertCurrencyUsingLookup converts using conversion rate in CurrencyConversion table]
+AS
+BEGIN
+
+exec tsqlt.Fail "Net yet implemented"
+
+END
