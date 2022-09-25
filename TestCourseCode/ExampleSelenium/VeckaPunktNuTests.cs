3241be880508f8e816eef71add88d0d68b6d3f00
Mikael Waldenmark <mikael.waldenmark@amf.se>
2022-09-25 14:20:33 +0200

    Lade till exempel på Selenium

---
+++ TestCourseCode/ExampleSelenium/VeckaPunktNuTests.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ExampleSelenium
+{
+    public class VeckaPunktNuTests
+    {
+        [Fact]
+        public void TestSkaLäsaNuvarandeVeckaFrånVeckaPunktNu()
+        {
+
+                /*ChromeOptions options = new ChromeOptions();
+
+                options.AddArguments(new List<string>()
+                {
+                    "--silent-launch",
+                    "--no-startup-window",
+                    "no-sandbox",
+                    "headless"
+                });*/
+                IWebDriver driver = new ChromeDriver();
+                String veckonummer = "";
+                driver.Close();
+                Assert.Equal("39", veckonummer);
+
+        }
+    }
+}
+
