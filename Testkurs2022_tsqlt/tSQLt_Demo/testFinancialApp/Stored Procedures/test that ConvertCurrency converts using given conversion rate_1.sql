b4e5a4d66a6eb2e7feeafccf1a3f8ec75a49e9f4
Mikael Waldenmark <mikael.waldenmark@amf.se>
2022-09-25 11:19:02 +0200

    Lade till mall för första testfall i tsqlt

--- Testkurs2022_tsqlt/tSQLt_Demo/testFinancialApp/Stored Procedures/test that ConvertCurrency converts using given conversion rate_1.sql
+++ Testkurs2022_tsqlt/tSQLt_Demo/testFinancialApp/Stored Procedures/test that ConvertCurrency converts using given conversion rate_1.sql
@@ -1,13 +1,7 @@
 ﻿CREATE PROCEDURE testFinancialApp.[test that ConvertCurrency converts using given conversion rate]
 AS
 BEGIN
-    DECLARE @actual MONEY;
-    DECLARE @rate DECIMAL(10,4); SET @rate = 1.2;
-    DECLARE @amount MONEY; SET @amount = 2.00;
-
-    SELECT @actual = FinancialApp.ConvertCurrency(@rate, @amount);
-
-    DECLARE @expected MONEY; SET @expected = 2.4;   --(rate * amount)
-    EXEC tSQLt.assertEquals @expected, @actual;
+    
+    exec tsqlt.Fail "Not yet implemented"
 
 END;
