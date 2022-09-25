27704a65e845b815f036a739c0cb5e1988bd5a22
Mikael Waldenmark <mikael.waldenmark@amf.se>
2022-09-25 16:37:40 +0200

    Byggde på exempel med specflow

---
+++ TestCourseCode/ExampleSpecflow/Calculator/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleSpecflow.Calculator
+{
+    public class Calculator
+    {
+        public int sum(int firstNumber, int secondNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TestCourseCode/ExampleSpecflow/StepDefinitions/CalculatorStepDefinitions.cs
+++ TestCourseCode/ExampleSpecflow/StepDefinitions/CalculatorStepDefinitions.cs
@@ -3,42 +3,39 @@ namespace ExampleSpecflow.StepDefinitions
     [Binding]
     public sealed class CalculatorStepDefinitions
     {
+        private int _sum;
+
+        private int _secondNumber;
+
+        private int _firstNumber;
+
+        private Calculator.Calculator _calculator = new Calculator.Calculator();
         // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
 
         [Given("the first number is (.*)")]
         public void GivenTheFirstNumberIs(int number)
         {
-            //TODO: implement arrange (precondition) logic
-            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
-            // To use the multiline text or the table argument of the scenario,
-            // additional string/Table parameters can be defined on the step definition
-            // method. 
-
-            throw new PendingStepException();
+            _firstNumber = number;
         }
 
         [Given("the second number is (.*)")]
         public void GivenTheSecondNumberIs(int number)
         {
-            //TODO: implement arrange (precondition) logic
-
-            throw new PendingStepException();
+            _secondNumber = number;
         }
 
         [When("the two numbers are added")]
         public void WhenTheTwoNumbersAreAdded()
         {
-            //TODO: implement act (action) logic
-
-            throw new PendingStepException();
+            _sum = _calculator.sum(_firstNumber, _secondNumber);
         }
 
         [Then("the result should be (.*)")]
         public void ThenTheResultShouldBe(int result)
         {
-            //TODO: implement assert (verification) logic
-
-            throw new PendingStepException();
+            Assert.Equal(result, _sum);
         }
     }
+
+
 }
